21f51db13f96a04d1d10e5471ca7826f0bda414f
Dinuka Dilshan <73058516+Dinuka-Dilshan@users.noreply.github.com>
2021-08-18 17:05:30 +0530

    customize bootsrap

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./public/css/main.min.css">
+    <title>ShineWay|Login</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">ShineWay</a>
+        </div>
+    </nav>
+
+    <div class="row vh-90">
+        <div class="col-2 bg-gray-400 ">
+
+        </div>
+        <div class="col-10 ">
+
+        </div>
+    </div>
+</body>
+
+</html>
