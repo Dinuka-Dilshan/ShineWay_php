d8c92b2e0cac0994a3b2d1623147acb189f78d70
Dinuka Dilshan <73058516+Dinuka-Dilshan@users.noreply.github.com>
2021-08-21 13:33:54 +0530

    user edit modal

--- views/user-view.php
+++ views/user-view.php
@@ -66,7 +66,13 @@ $userList = $_SESSION['userList'];
                                         </button>
                                     </td>
                                 </form>
-
+                                <!--<form action="../controllers/user-edit-controller.php" method="post">-->
+                                <td>
+                                    <button data-mdb-toggle="modal" data-mdb-target="#modal2" name="submit-edit-user" value="<?php echo $user['ID'] ?>" type="submit" class="btn btn-secondary btn-sm px-3">
+                                        <i class="fas fa-user-edit"></i>
+                                    </button>
+                                </td>
+                                <!--</form>-->
                             </tr>
                             <?php $countTable++ ?>
                         <?php endforeach ?>
@@ -108,17 +114,139 @@ $userList = $_SESSION['userList'];
     </div>
 </div>
 
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.6.0/mdb.min.js"></script>
 
+
+
+<!-- Modal edit user-->
+<div class="modal fade" id="modal2" data-mdb-backdrop="static" data-mdb-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog ">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="Modal-edit-title" >Modal title</h5>
+                <button type="button" class="btn-close" data-mdb-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col">
+                            <form class=" needs-validation" novalidate>
+                                <div class="row mb-4">
+                                    <div class="col-12">
+                                        <div class="form-outline">
+                                            <input id="Modal-edit-name" type="text" id="form6Example1" class="form-control" required />
+                                            <label class="form-label" for="form6Example1">Name</label>
+                                            <div class="valid-feedback">Looks good!</div>
+                                            <div class="invalid-feedback">Enter a valid name</div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="row mb-4">
+                                    <div class="col">
+                                        <div class="form-outline">
+                                            <input id="Modal-edit-nic" type="text" id="form6Example2" class="form-control" required />
+                                            <label class="form-label" for="form6Example2">NIC</label>
+                                            <div class="valid-feedback">Looks good!</div>
+                                            <div class="invalid-feedback">Enter a valid NIC</div>
+                                        </div>
+                                    </div>
+                                </div>
+
+
+                                <div class="row mb-4">
+                                    <div class="col">
+                                        <div class="form-outline ">
+                                            <input id="Modal-edit-address" type="text" id="form6Example4" class="form-control" required />
+                                            <label class="form-label" for="form6Example4">Address</label>
+                                            <div class="valid-feedback ">Looks good!</div>
+                                            <div class="invalid-feedback ">Enter a valid Address</div>
+                                        </div>
+                                    </div>
+                                </div>
+
+
+                                <!-- Email input -->
+                                <div class="row mb-4">
+                                    <div class="col">
+                                        <div class="form-outline ">
+                                            <input id="Modal-edit-email"  type="email" id="form6Example5" class="form-control" required />
+                                            <label class="form-label" for="form6Example5">Email</label>
+                                            <div class="valid-feedback">Looks good!</div>
+                                            <div class="invalid-feedback">Enter a valid Email</div>
+                                        </div>
+                                    </div>
+                                </div>
+
+
+                                <!-- Number input -->
+                                <div class="row mb-4">
+                                    <div class="col">
+                                        <div class="form-outline">
+                                            <input id="Modal-edit-phone" type="number" id="form6Example6" class="form-control" required />
+                                            <label class="form-label" for="form6Example6">Phone</label>
+                                            <div class="valid-feedback ">Looks good!</div>
+                                            <div class="invalid-feedback ">Enter a valid phone number</div>
+                                        </div>
+                                    </div>
+                                </div>
+
+
+                                <div class="bg-white mb-4 mt-1">
+                                    <label class="form-label bg-white " for="customFile">Image</label>
+                                    <input type="file" class="form-control bg-white " id="customFile" />
+                                </div>
+
+                                <select class="bg-white  form-control mb-4" required>
+                                    <option selected value="1">User Type - Admin </option>
+                                    <option value="1">User Type - User</option>
+                                </select>
+
+                                <!-- Submit button -->
+                                <button type="submit" class="btn btn-primary btn-block mb-4">Update User</button>
+                            </form>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-mdb-dismiss="modal">
+                    Close
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.6.0/mdb.min.js"></script>
+<script src="../public/js/form-validstion.js"></script>
 <!-- Modal image set -->
 <script>
     const table = document.getElementById('dataTable');
     let userImage = document.getElementById('user-image');
     const modalTitle = document.getElementById('staticBackdropLabel');
+    const modalEditName = document.getElementById('Modal-edit-name');
+    const modalEditNIC = document.getElementById('Modal-edit-nic');
+    const modalEditAddress = document.getElementById('Modal-edit-address');
+    const modalEditPhone = document.getElementById('Modal-edit-phone');
+    const modalEditEmail = document.getElementById('Modal-edit-email');
+    const modalEditTitle = document.getElementById('Modal-edit-title');
 
     function cellClickFire(x) {
         userImage.src = `../public/img/Users/${table.rows[x.rowIndex].cells[4].innerHTML}.jpg`;
         modalTitle.innerHTML = table.rows[x.rowIndex].cells[1].innerHTML;
+        modalEditName.value = table.rows[x.rowIndex].cells[1].innerHTML;
+        modalEditNIC.value = table.rows[x.rowIndex].cells[2].innerHTML;
+        modalEditAddress.value = table.rows[x.rowIndex].cells[6].innerHTML;
+        modalEditPhone.value = table.rows[x.rowIndex].cells[5].innerHTML;
+        modalEditEmail.value = table.rows[x.rowIndex].cells[4].innerHTML;
+        modalEditTitle.innerHTML = "Edit User: " + table.rows[x.rowIndex].cells[1].innerHTML;
     }
 </script>
 
