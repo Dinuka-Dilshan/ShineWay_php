8ab6d9071ea829982292cde5fd3e6713dbd52675
Dinuka Dilshan <73058516+Dinuka-Dilshan@users.noreply.github.com>
2021-08-22 21:24:58 +0530

    customer add

--- views/customer-add.php
+++ views/customer-add.php
@@ -9,32 +9,31 @@ $scriptSetValues;
 
 $error = -1; // -1 = no message to display 1 = successfull  0 = error 2 = email exists
 
-if (isset($_SESSION['userAddStatus'])) {
-    if ($_SESSION['userAddStatus'] == 1) {
+if (isset($_SESSION['customerAddStatus'])) {
+    if ($_SESSION['customerAddStatus'] == 1) {
         $error = 1;
-    } else if ($_SESSION['userAddStatus'] == 0) {
+    } else if ($_SESSION['customerAddStatus'] == 0) {
         $error = 0;
-    } else if($_SESSION['userAddStatus'] == 2){
+    } else if ($_SESSION['customerAddStatus'] == 2) {
         $error = 2;
-        $name =  $_SESSION['faildToAddName'] ;
-        $email = $_SESSION['faildToAddEmail'] ;
-        $phone =  $_SESSION['faildToAddPhone'] ;
+        $name =  $_SESSION['faildToAddName'];
+        $email = $_SESSION['faildToAddEmail'];
+        $phone =  $_SESSION['faildToAddPhone'];
         $address =  $_SESSION['faildToAddAddress'];
-        $NIC = $_SESSION['faildToAddNIC'] ;
-        $userType = $_SESSION['faildToAddUserType'] ;
-        $scriptSetValues = "setFiledsAfterFail('$name', '$phone', '$email', '$address', '$NIC');";
+        $NIC = $_SESSION['faildToAddNIC'];
+        $License  = $_SESSION['faildToAddlicense'];
+        $scriptSetValues = "setFiledsAfterFail('$name', '$phone', '$email', '$address', '$NIC', '$License');";
         unset($_SESSION['faildToAddName']);
-        unset($_SESSION['faildToAddEmail'] );
-        unset( $_SESSION['faildToAddPhone']);
+        unset($_SESSION['faildToAddEmail']);
+        unset($_SESSION['faildToAddPhone']);
         unset($_SESSION['faildToAddAddress']);
         unset($_SESSION['faildToAddNIC']);
-        unset($_SESSION['faildToAddUserType']);
-            
-    }else{
+        unset($_SESSION['faildToAddlicense']);
+    } else {
         $error = -1;
     }
 
-    unset($_SESSION['userAddStatus']);
+    unset($_SESSION['customerAddStatus']);
 }
 
 
@@ -50,11 +49,11 @@ if (isset($_SESSION['userAddStatus'])) {
                     ADD USER
                 </div>
                 <div class="card-body">
-                    <form enctype="multipart/form-data" class=" needs-validation" novalidate action="../controllers/user-add-controller.php" method="POST">
+                    <form  class=" needs-validation" novalidate action="../controllers/customer-add-controller.php" method="POST">
                         <div class="row mb-4">
                             <div class="col-12">
                                 <div class="form-outline">
-                                    <input pattern="^(?![ .]+$)[a-zA-Z .]*" id="name" name="name" type="text"  class="form-control" required />
+                                    <input pattern="^(?![ .]+$)[a-zA-Z .]*" id="name" name="name" type="text" class="form-control" required />
                                     <label class="form-label" for="form6Example1">Name</label>
                                     <div class="valid-feedback">Looks good!</div>
                                     <div class="invalid-feedback">Enter a valid name</div>
@@ -65,7 +64,7 @@ if (isset($_SESSION['userAddStatus'])) {
                         <div class="row mb-4">
                             <div class="col">
                                 <div class="form-outline">
-                                    <input pattern="([0-9]{9}[x|X|v|V]|[0-9]{12})" id="nic" name="NIC" type="text"  class="form-control" required />
+                                    <input pattern="([0-9]{9}[x|X|v|V]|[0-9]{12})" id="nic" name="NIC" type="text" class="form-control" required />
                                     <label class="form-label" for="form6Example2">NIC</label>
                                     <div class="valid-feedback">Looks good!</div>
                                     <div class="invalid-feedback">Enter a valid NIC</div>
@@ -76,7 +75,7 @@ if (isset($_SESSION['userAddStatus'])) {
                         <div class="row mb-4">
                             <div class="col">
                                 <div class="form-outline ">
-                                    <input pattern="^(?![0-9]+$)[a-zA-Z0-9 ,]{2,}$" name="address" id="address" type="text"  class="form-control" required />
+                                    <input pattern="^(?![0-9]+$)[a-zA-Z0-9 ,]{2,}$" name="address" id="address" type="text" class="form-control" required />
                                     <label class="form-label" for="form6Example4">Address</label>
                                     <div class="valid-feedback ">Looks good!</div>
                                     <div class="invalid-feedback ">Enter a valid Address</div>
@@ -112,21 +111,17 @@ if (isset($_SESSION['userAddStatus'])) {
 
 
                         <div class="row mb-4">
-                            <div class="col-12 ">
-                                <div class="bg-white ">
-                                    <label class="form-label bg-white mb-0 pb-0" for="customFile">Image</label>
-                                    <input required name="image" type="file" class="form-control bg-white " id="file" accept="image/*" />
+
+                            <div class="col">
+                                <div class="form-outline">
+                                    <input pattern="^[A-Z]{1}[1-9]{7,8}[A-Z]{0,1}$" name="license" id="license" type="text" class="form-control" required />
+                                    <label class="form-label" for="form6Example6">License</label>
+                                    <div class="valid-feedback ">Looks good!</div>
+                                    <div class="invalid-feedback ">Enter a valid licence number</div>
                                 </div>
                             </div>
 
 
-                            <div class="col-12 mt-2">
-                                <label class="form-label bg-white mb-0 pb-0 " for="typeSelect">User Type</label>
-                                <select name="userType" class="bg-white  form-control " id="typeSelect" required>
-                                    <option selected value="Admin"> Admin </option>
-                                    <option value="User"> User</option>
-                                </select>
-                            </div>
 
                         </div>
 
@@ -136,7 +131,7 @@ if (isset($_SESSION['userAddStatus'])) {
                                 <button type="reset" id="btnReset" class="btn btn-primary btn-block fs-6 py-2 mb-2">Reset</button>
                             </div>
                             <div class="col-6">
-                                <button type="submit" name="submit-add-user" class="btn btn-primary btn-block fs-6 py-2 mb-2">Add User</button>
+                                <button type="submit" name="submit-add-customer" class="btn btn-primary btn-block fs-6 py-2 mb-2">Add User</button>
                             </div>
                         </div>
                     </form>
@@ -224,7 +219,7 @@ if (isset($_SESSION['userAddStatus'])) {
                     <div class="row">
 
                         <div class="col-10">
-                            <h6 class="modal-title text-danger " id="staticBackdropLabel"> Email Already Exists!</h6>
+                            <h6 class="modal-title text-danger " id="staticBackdropLabel"> NIC Already Exists!</h6>
                         </div>
 
                         <div class="col-2 ">
@@ -245,30 +240,28 @@ if (isset($_SESSION['userAddStatus'])) {
 <script src="../public/js/form-validstion.js"></script>
 
 <script>
-    function getByID(elementID){
+    function getByID(elementID) {
         return document.getElementById(elementID);
     }
 
-    function setFiledsAfterFail(name,phone,email,address,nic){
+    function setFiledsAfterFail(name, phone, email, address, nic,license) {
         getByID('name').value = name;
         getByID('phone').value = phone;
         getByID('email').value = email;
         getByID('address').value = address;
         getByID('nic').value = nic;
+        getByID('license').value = license;
     }
     <?php
-        if ($error == "0") {
-            echo "document.getElementById('btnErrorModal').click();";
-        } else if ($error == "1") {
-            echo "document.getElementById('btnModal').click();";
-        } else if($error == "2") {
-            echo "document.getElementById('btnErrorEmailModal').click();";
-            echo $scriptSetValues;
-            
-        }
+    if ($error == "0") {
+        echo "document.getElementById('btnErrorModal').click();";
+    } else if ($error == "1") {
+        echo "document.getElementById('btnModal').click();";
+    } else if ($error == "2") {
+        echo "document.getElementById('btnErrorEmailModal').click();";
+        echo $scriptSetValues;
+    }
     ?>
-
-    
 </script>
 
 <?php
