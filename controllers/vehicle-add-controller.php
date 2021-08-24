632b526b7c51807dfe33a3ae2bf0889f415ded0b
Dinuka Dilshan <73058516+Dinuka-Dilshan@users.noreply.github.com>
2021-08-24 14:54:34 +0530

    vehicle add save images

--- controllers/vehicle-add-controller.php
+++ controllers/vehicle-add-controller.php
@@ -33,31 +33,48 @@ if (isset($_POST['submit-add-vehicle'])) {
         header('Location: ../views/vehicle-add.php');
     }else{
         $vehicleNumberCheck = "SELECT `Vehicle_num` FROM `vehicle` WHERE `Vehicle_num` = '$vehicleNumber';";
-    $result = $connection->query($vehicleNumberCheck);
+        $result = $connection->query($vehicleNumberCheck);
 
-    if($result->num_rows != 0){
-        $_SESSION['vehicleAddStatus'] = 2;
-        $_SESSION['faildToAddVehicleNumber'] = $_POST['vehicleNumber'];
-        $_SESSION['faildToAddBrand'] = $_POST['Brand'];
-        $_SESSION['faildToAddModel'] = $_POST['model'];
-        $_SESSION['faildToAddNIC'] = $_POST['address'];
-        $_SESSION['faildToAddNIC'] = $_POST['NIC'];
-        $_SESSION['faildToAddDailyPrice'] = $_POST['dailyPrice'];
-        $_SESSION['faildToAddWeeklyPrice'] = $_POST['weeklyPrice'];
-        $_SESSION['faildToAddMonthlyPrice'] = $_POST['monthlyPrice'];
-        $_SESSION['faildToAddOwnerPrice'] = $_POST['ownerPrice'];
-        $_SESSION['faildToAddVehicleType'] = $_POST['vehicleType'];
-    }else{
-        $query = "INSERT INTO `vehicle`(`Vehicle_num`, `Brand`, `Model`, `Type`, `Owner_NIC`, `Daily_price`, `Weekly_price`,`Monthly_price`,  `Owner_payment`) VALUES ('$vehicleNumber','$brand','$model','$vehicleType','$NIC','$dailyPrice','$weeklyPrice','$monthlyPrice','$ownerPrice')";
+        if($result->num_rows != 0){
+            $_SESSION['vehicleAddStatus'] = 2;
+            $_SESSION['faildToAddVehicleNumber'] = $_POST['vehicleNumber'];
+            $_SESSION['faildToAddBrand'] = $_POST['Brand'];
+            $_SESSION['faildToAddModel'] = $_POST['model'];
+            $_SESSION['faildToAddNIC'] = $_POST['address'];
+            $_SESSION['faildToAddNIC'] = $_POST['NIC'];
+            $_SESSION['faildToAddDailyPrice'] = $_POST['dailyPrice'];
+            $_SESSION['faildToAddWeeklyPrice'] = $_POST['weeklyPrice'];
+            $_SESSION['faildToAddMonthlyPrice'] = $_POST['monthlyPrice'];
+            $_SESSION['faildToAddOwnerPrice'] = $_POST['ownerPrice'];
+            $_SESSION['faildToAddVehicleType'] = $_POST['vehicleType'];
+        }else{
+            $query = "INSERT INTO `vehicle`(`Vehicle_num`, `Brand`, `Model`, `Type`, `Owner_NIC`, `Daily_price`, `Weekly_price`,`Monthly_price`,  `Owner_payment`) VALUES ('$vehicleNumber','$brand','$model','$vehicleType','$NIC','$dailyPrice','$weeklyPrice','$monthlyPrice','$ownerPrice')";
 
-        $result = $connection->query($query);
-    
-        if ($result) {
-            $_SESSION['vehicleAddStatus'] = 1;
-        } else {
-            $_SESSION['vehicleAddStatus'] = 0;
+            $result = $connection->query($query);
+        
+            if ($result) {
+                $_SESSION['vehicleAddStatus'] = 1;
+
+                if($_FILES['insideImage']['name']!=''){
+                    $target_file = "../public/img/Vehicles/". $_POST['vehicleNumber']."-inside.jpg";
+                    if(file_exists($target_file)){
+                        unlink($target_file);
+                    }
+                    move_uploaded_file($_FILES["insideImage"]["tmp_name"], $target_file);
+                }
+
+                if($_FILES['overallImage']['name']!=''){
+                    $target_file = "../public/img/Vehicles/". $_POST['vehicleNumber']."-overall.jpg";
+                    if(file_exists($target_file)){
+                        unlink($target_file);
+                    }
+                    move_uploaded_file($_FILES["overallImage"]["tmp_name"], $target_file);
+                }
+
+            } else {
+                $_SESSION['vehicleAddStatus'] = 0;
+            }
         }
-    }
     
     }
 
